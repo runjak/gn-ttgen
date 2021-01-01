c1a64131612e40415eca1e54649bf9077e1785a4
Jakob Runge <github@runjak.codes>
2021-01-01 02:38:32 +0100

    open browser with puppeteer

--- src/index.js
+++ src/index.js
@@ -1,2 +1,20 @@
-console.log('Hello Gnöttgen!');
+const puppeteer = require('puppeteer');
 
+const entryPoint = 'https://engelsystem.de/a38';
+const headless = false;
+
+const sleep = (ms) => new Promise((resolve) => { setTimeout(resolve, ms) });
+const second = 1000;
+const minute = 60 * second;
+
+const main = async () => {
+  const browser = await puppeteer.launch({ headless });
+  const page = await browser.newPage();
+  await page.goto(entryPoint);
+
+  await sleep(5 * minute);
+
+  await browser.close();
+};
+
+main();
