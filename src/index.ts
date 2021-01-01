ae02e66c41e018071b013dbf2418bb9a517dfb4e
Jakob Runge <github@runjak.codes>
2021-01-01 03:02:13 +0100

    use prettier

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,20 @@
-import { launch } from 'puppeteer';
-import { Page } from 'puppeteer/lib/cjs/puppeteer/common/Page';
+import { launch } from "puppeteer";
+import { Page } from "puppeteer/lib/cjs/puppeteer/common/Page";
 
-const entryPoint = 'https://engelsystem.de/a38';
+const entryPoint = "https://engelsystem.de/a38";
 const headless = false;
 
-const startButtonSelector = 'a.button.start';
+const startButtonSelector = "a.button.start";
 
 const clickStartButton = async (page: Page) => {
   await page.waitForSelector(startButtonSelector);
   await page.click(startButtonSelector);
 };
 
-const getTasks = (page: Page): Promise<Array<string>> => (
-  page.$$eval('.tasklist .task', (elements) => (elements.map(element => element.innerHTML)))
-);
+const getTasks = (page: Page): Promise<Array<string>> =>
+  page.$$eval(".tasklist .task", (elements) =>
+    elements.map((element) => element.innerHTML)
+  );
 
 const main = async () => {
   const browser = await launch({ headless });
@@ -23,7 +24,7 @@ const main = async () => {
   await clickStartButton(page);
   await clickStartButton(page);
   const tasks = await getTasks(page);
-  console.log('current tasks are:', JSON.stringify(tasks));
+  console.log("current tasks are:", JSON.stringify(tasks));
 
   await browser.close();
 };
