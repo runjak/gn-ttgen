2d7670d3f11076f22065f5a9356fc54d6516822c
Jakob Runge <github@runjak.codes>
2021-01-02 00:11:44 +0100

    add interrogateAll

--- src/index.ts
+++ src/index.ts
@@ -13,6 +13,7 @@ const selectors = {
   downstairs: '.world [href]>img[src="img/downstairs_00.png"]',
   employee: '.world [href]>img[src^="img/employee_"]',
   infotext: ".infotext",
+  dialogTask: ".dialog>a",
   byHref: (href: string) => `.world [href="${href}"]`,
 } as const;
 
@@ -31,32 +32,30 @@ const tasksDone = (tasks: Array<string>): boolean => {
   return tasks.length === 1 && tasks[0] === "Kehre nach draußen zurück.";
 };
 
-type Person = {
+type Employee = {
   readonly name: string | null;
-  readonly tasks: Array<string>;
   readonly href: string;
 };
 
 type Room = {
-  readonly discovered: boolean;
-  readonly isStart: boolean;
   readonly signs: Array<string>;
-  readonly persons: Array<Person>;
+  readonly employees: Array<Employee>;
   readonly linkHrefs: Array<string>;
-};
-
-const startRoom: Room = {
-  discovered: false,
-  isStart: true,
-  signs: [],
-  persons: [],
-  linkHrefs: [],
 };
 
 const parentHrefs = (page: Page, selector: string): Promise<Array<string>> =>
   page.$$eval(selector, (elements) =>
     elements.map((element) => element.parentElement.getAttribute("href"))
   );
+
+const getInfotext = async (page: Page): Promise<string> => {
+  const text = await page.$eval(
+    selectors.infotext,
+    // @ts-ignore .innerText exists
+    (element: Element): string => element?.innerText ?? ""
+  );
+  return text.trim();
+};
 
 const readSigns = async (
   page: Page,
@@ -66,25 +65,61 @@ const readSigns = async (
 
   for (const href of hrefs) {
     await click(page, selectors.byHref(href));
-    const text = await page.$eval(
-      selectors.infotext,
-      // @ts-ignore .innerText exists
-      (element: Element): string => element?.innerText ?? ""
-    );
-    signs.push(text.trim());
+    const text = await getInfotext(page);
+    signs.push(text);
   }
 
   return signs;
 };
 
-const discoverRoom = async (page: Page, room: Room): Promise<Room> => {
-  if (room.discovered) {
-    return room;
+const getTaskButtonLabel = async (page: Page): Promise<string> => {
+  try {
+    const label = await page.$eval(
+      selectors.dialogTask,
+      // @ts-ignore .innerText exists
+      (element: Element): string => element?.innerText ?? ""
+    );
+    return label;
+  } catch (e) {
+    return "";
+  }
+};
+
+const checkTasks = async (page: Page) => {
+  let previousLabel = "";
+  let currentLabel = await getTaskButtonLabel(page);
+
+  while (currentLabel !== '' && previousLabel !== currentLabel) {
+    await click(page, selectors.dialogTask);
+    const answer = await getInfotext(page);
+    console.log(currentLabel, "->", answer);
+
+    previousLabel = currentLabel;
+    currentLabel = await getTaskButtonLabel(page);
+  }
+};
+
+const interrogateAll = async (
+  page: Page,
+  employeeHrefs: Array<string>
+): Promise<Array<Employee>> => {
+  let employees: Array<Employee> = [];
+
+  for (const href of employeeHrefs) {
+    await click(page, selectors.byHref(href));
+    const text = await getInfotext(page);
+    const [name] = text.split(":");
+
+    await checkTasks(page);
+
+    employees.push({ href, name });
   }
 
-  const signHrefPromise = room.isStart
-    ? parentHrefs(page, selectors.signs)
-    : ([] as Array<string>);
+  return employees;
+};
+
+const discoverRoom = async (page: Page): Promise<Room> => {
+  const signHrefPromise = parentHrefs(page, selectors.signs);
   const linkHrefPromise = parentHrefs(
     page,
     [selectors.door, selectors.upstairs, selectors.downstairs].join(",")
@@ -97,20 +132,22 @@ const discoverRoom = async (page: Page, room: Room): Promise<Room> => {
   ]);
 
   const signs = await readSigns(page, signHrefs);
+  const employees = await interrogateAll(page, employeeHrefs);
   console.log("stuff found in room:");
-  console.log("employees:", employeeHrefs.join(" "));
+  console.log("employees:", employees.map((e) => JSON.stringify(e)).join(" "));
 
   return {
-    ...room,
-    discovered: true,
     signs,
     linkHrefs,
+    employees,
   };
 };
 
 const playLevel = async (page: Page) => {
   await click(page, selectors.startButton);
-  const discoveredStart = discoverRoom(page, startRoom);
+  const discoveredStart = await discoverRoom(page);
+  await click(page, selectors.byHref(discoveredStart.linkHrefs[0]));
+  const secondRoom = await discoverRoom(page);
 };
 
 const main = async () => {
