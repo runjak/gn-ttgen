412f0e7920253774500568d31a62d66d5bbc1c15
Jakob Runge <github@runjak.codes>
2021-01-01 02:59:28 +0100

    fetch initial tasks

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,29 @@
-import puppeteer from 'puppeteer';
+import { launch } from 'puppeteer';
+import { Page } from 'puppeteer/lib/cjs/puppeteer/common/Page';
 
 const entryPoint = 'https://engelsystem.de/a38';
 const headless = false;
 
-const sleep = (ms) => new Promise((resolve) => { setTimeout(resolve, ms) });
-const second = 1000;
-const minute = 60 * second;
+const startButtonSelector = 'a.button.start';
+
+const clickStartButton = async (page: Page) => {
+  await page.waitForSelector(startButtonSelector);
+  await page.click(startButtonSelector);
+};
+
+const getTasks = (page: Page): Promise<Array<string>> => (
+  page.$$eval('.tasklist .task', (elements) => (elements.map(element => element.innerHTML)))
+);
 
 const main = async () => {
-  const browser = await puppeteer.launch({ headless });
+  const browser = await launch({ headless });
   const page = await browser.newPage();
   await page.goto(entryPoint);
 
-  await sleep(5 * minute);
+  await clickStartButton(page);
+  await clickStartButton(page);
+  const tasks = await getTasks(page);
+  console.log('current tasks are:', JSON.stringify(tasks));
 
   await browser.close();
 };
