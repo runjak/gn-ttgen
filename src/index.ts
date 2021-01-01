3b4f99708fac8346c4ad6944c8023f2ee7113454
Jakob Runge <github@runjak.codes>
2021-01-01 03:17:37 +0100

    enter first building

--- src/index.ts
+++ src/index.ts
@@ -4,17 +4,31 @@ import { Page } from "puppeteer/lib/cjs/puppeteer/common/Page";
 const entryPoint = "https://engelsystem.de/a38";
 const headless = false;
 
-const startButtonSelector = "a.button.start";
+const selectors = {
+  startButton: "a.button.start",
+  tasks: ".tasklist .task",
+  actions: ".world [href]",
+} as const;
+
+const sleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
+const second = 1000;
 
 const clickStartButton = async (page: Page) => {
-  await page.waitForSelector(startButtonSelector);
-  await page.click(startButtonSelector);
+  await page.waitForSelector(selectors.startButton);
+  await page.click(selectors.startButton);
 };
 
 const getTasks = (page: Page): Promise<Array<string>> =>
-  page.$$eval(".tasklist .task", (elements) =>
+  page.$$eval(selectors.tasks, (elements) =>
     elements.map((element) => element.innerHTML)
   );
+
+const getCurrentActions = async (page: Page) => {
+  console.log("getCurrentActions");
+  await page.waitForSelector(selectors.actions);
+  console.log("selecting…");
+  return page.$$(selectors.actions);
+};
 
 const main = async () => {
   const browser = await launch({ headless });
@@ -23,8 +37,14 @@ const main = async () => {
 
   await clickStartButton(page);
   await clickStartButton(page);
+
   const tasks = await getTasks(page);
   console.log("current tasks are:", JSON.stringify(tasks));
+
+  const [action] = await getCurrentActions(page);
+  console.log("clicking!");
+  await action.click();
+  await sleep(30 * second);
 
   await browser.close();
 };
